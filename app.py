38084307f94c037829b41e7b290c844918cdc282
Shridhar <shridharshatrughan2001@gmail.com>
2022-10-14 15:03:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,17 @@ app.app_context().push()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+#cartitem1 = CartItem(id = 1, name = "Apple", cost = 10)
+#cartitem2 = CartItem(id = 2, name = "Banana", cost = 5)
+#cartitem3 = CartItem(id = 3, name = "Orange", cost = 8)
+#cartitem4 = CartItem(id = 4, name = "Watermelon", cost = 20)
+        
+#db.session.add(cartitem1)
+#db.session.add(cartitem2)
+#db.session.add(cartitem3))
+#db.session.add(cartitem4)
+#db.session.commit()
+
 class CartItem(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
@@ -19,31 +30,16 @@ class CartItem(db.Model):
     def __repr__(self):
         return '<CartItem %r>' % self.id
 
-cartitem1 = CartItem(id = 1, name = "Apple", cost = 10)
-cartitem2 = CartItem(id = 2, name = "Banana", cost = 5)
-cartitem3 = CartItem(id = 3, name = "Orange", cost = 8)
-cartitem4 = CartItem(id = 4, name = "Watermelon", cost = 20)
-
-try:
-    db.session.add(cartitem1)
-    db.session.add(cartitem2)
-    db.session.add(cartitem3)
-    db.session.add(cartitem4)
-    db.session.commit()
-except:
-    print("Database exists")
-
-
-
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cartitems = CartItem.query.all()
+    return render_template('index.html', cartitems=cartitems)
 
     
-@app.route('/add/<int:id>', methods=['GET', 'POST'])
+@app.route('/add/<int:id>', methods=['POST'])
 def add(id):
-    # returns the cart item which was just added to
+    # addedcartitem is the cart item which was just added to
     addedcartitem = CartItem.query.get_or_404(id)
 
     if request.method == 'POST':
@@ -51,7 +47,11 @@ def add(id):
 
         try:
             db.session.commit()
-            
+            return redirect('/')
+        except:
+            return "There was an error adding inventory"
+    
+    return "Error in add route"
 
 
 
